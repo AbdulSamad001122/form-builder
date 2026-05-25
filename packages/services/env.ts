a40311f643b4d380e517ce96ee9db9c67a84633d
Abdul Samad <iamabdulsamad2.0@gmail.com>
2026-05-25 11:11:01 +0500

    Fix : Removed the hardcoded cloudinary credentials and also rotated the credentials

--- packages/services/env.ts
+++ packages/services/env.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 const envSchema = z.object({
     JWT_SECRET: z.string().describe("JWT secret for signing tokens"),
     RESEND_API_KEY: z.string().describe("Resend email service API Key"),
-    CLOUDINARY_CLOUD_NAME: z.string().default("dubngcmbl"),
-    CLOUDINARY_API_KEY: z.string().default("115215566627279"),
-    CLOUDINARY_API_SECRET: z.string().default("7dmG20-y6hmjwPrRm7hX1wnt-9M"),
+    CLOUDINARY_CLOUD_NAME: z.string().describe("Cloudinary Cloud Name"),
+    CLOUDINARY_API_KEY: z.string().describe("Cloudinary API Key"),
+    CLOUDINARY_API_SECRET: z.string().describe("Cloudinary API Secret"),
     CLOUDINARY_FOLDER: z.string().default("for-form-builder-logos"),
 });
 
